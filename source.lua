2316a5f52030a7d0f2218d3e66c32c944fbedd1c
ryleebrooo <blippiecantgivemeafreshacc@gmail.com>
2025-11-07 01:26:05 +0000

    Update source.lua

--- source.lua
+++ source.lua
@@ -66,7 +66,7 @@
 
 -- library init
 	local library = {
-		directory = "",
+		directory = "synthetic",
 		folders = {
 			"/fonts",
 			"/configs",
@@ -1525,7 +1525,7 @@
 
 			-- main window
 				local main_window = library:panel({
-					name = properties and properties.name or "Synthetic | ", 
+					name = properties and properties.name or "synthetic | ", 
 					size = dim2(0, 604, 0, 628),
 					position = dim2(0, (camera.ViewportSize.X / 2) - 302 - 96, 0, (camera.ViewportSize.Y / 2) - 421 - 12),
 					image = "rbxassetid://98823308062942",
@@ -1630,11 +1630,11 @@
 					image = "rbxassetid://115194686863276",
 				})
 
-				local watermark = library:watermark({default = os.date('Synthetic |  - %b %d %Y - %H:%M:%S')})  
+				local watermark = library:watermark({default = os.date('synthetic |  - %b %d %Y - %H:%M:%S')})  
 
 				task.spawn(function()
 					while task.wait(1) do 
-						watermark.change_text(os.date('Synthetic - Beta - %b %d %Y - %H:%M:%S'))
+						watermark.change_text(os.date('synthetic - Beta - %b %d %Y - %H:%M:%S'))
 					end 
 				end) 
 
